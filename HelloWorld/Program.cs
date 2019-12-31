a6e148688ca4947c2af072faa7a074843118c220
shra1308 <shra.1308@gmail.com>
2019-12-30 21:30:54 -0700

    Added addition logic

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a + b);
             Console.WriteLine("Hello World Ravinder and Shilpi");
             Console.WriteLine("Hello World shra");
             Console.ReadLine();
